868639e3ed8d27ac1a01dea537bc25fe87607ea7
the-Shallow <theshallow27@gmail.com>
2022-03-11 17:49:57 +0530

    Fixed Warnings

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,7 +7,9 @@ import { toast } from "react-toastify";
 
 function Contact() {
     const [message, setMessage] = useState('');
-    const [landlord, setLandlord] = useState(null);
+  const [landlord, setLandlord] = useState(null);
+  
+  // eslint-disable-next-line
     const [searchParams, setSearchParams] = useSearchParams();
 
     const params = useParams();
